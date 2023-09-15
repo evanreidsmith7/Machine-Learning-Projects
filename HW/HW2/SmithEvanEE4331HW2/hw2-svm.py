febea98c9ca5d4140ca4eb9b7bf1b0f67a753e8b
evanreidsmith7 <evanreidsmith7@gmail.com>
2023-09-14 22:23:43 -0500

    got all the data

--- HW/HW2/SmithEvanEE4331HW2/hw2-svm.py
+++ HW/HW2/SmithEvanEE4331HW2/hw2-svm.py
@@ -30,6 +30,10 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import csv
 import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 # * labels *
 # corridor_rm155_7.1
 # Lab139_7.1
@@ -52,11 +56,11 @@ for filename in os.listdir(Corridor_rm155_71_loc0000_path):
         data = pd.read_csv(file_path)
         data = data['# Version 1.00'].str.split(';', expand=True)
         data = data.drop([0,1])
-        combined_data = pd.concat([combined_data, data], ignore_index=True)
+        combined_data1 = pd.concat([combined_data, data], ignore_index=True)
 
 # add the labels to the dataframe
-combined_data['label'] = 'Corridor'
-combined_data.to_csv("datasets\combined_data_Corridor_rm155_71_loc0000.csv", index=False)
+combined_data1['label'] = 'Corridor'
+combined_data1.to_csv("datasets\combined_data_Corridor_rm155_71_loc0000.csv", index=False)
 
 # Loop through each file in the folder and read the data into a dataframe
 for filename in os.listdir(Lab139_71_loc0000_path):
@@ -65,10 +69,10 @@ for filename in os.listdir(Lab139_71_loc0000_path):
         data = pd.read_csv(file_path)
         data = data['# Version 1.00'].str.split(';', expand=True)
         data = data.drop([0,1])
-        combined_data = pd.concat([combined_data, data], ignore_index=True)
+        combined_data2 = pd.concat([combined_data, data], ignore_index=True)
 
-combined_data['label'] = 'Lab139'
-combined_data.to_csv("datasets\combined_data_Lab139_71_loc0000.csv", index=False)
+combined_data2['label'] = 'Lab139'
+combined_data2.to_csv("datasets\combined_data_Lab139_71_loc0000.csv", index=False)
 
 # Loop through each file in the folder and read the data into a dataframe
 for filename in os.listdir(Main_Lobby71_loc0000_path):
@@ -77,10 +81,10 @@ for filename in os.listdir(Main_Lobby71_loc0000_path):
         data = pd.read_csv(file_path)
         data = data['# Version 1.00'].str.split(';', expand=True)
         data = data.drop([0,1])
-        combined_data = pd.concat([combined_data, data], ignore_index=True)
+        combined_data3 = pd.concat([combined_data, data], ignore_index=True)
 
-combined_data['label'] = 'Main_Lobby'
-combined_data.to_csv("datasets\combined_data_Main_Lobby71_loc0000.csv", index=False)
+combined_data3['label'] = 'Main_Lobby'
+combined_data3.to_csv("datasets\combined_data_Main_Lobby71_loc0000.csv", index=False)
 
 # Loop through each file in the folder and read the data into a dataframe
 for filename in os.listdir(Sport_Hall_71_loc0000_path):
@@ -89,38 +93,67 @@ for filename in os.listdir(Sport_Hall_71_loc0000_path):
         data = pd.read_csv(file_path)
         data = data['# Version 1.00'].str.split(';', expand=True)
         data = data.drop([0,1])
-        combined_data = pd.concat([combined_data, data], ignore_index=True)
+        combined_data4 = pd.concat([combined_data, data], ignore_index=True)
 
-combined_data['label'] = 'Sport_Hall'
-combined_data.to_csv("datasets\combined_data_Sport_Hall_71_loc0000.csv", index=False)
+combined_data4['label'] = 'Sport_Hall'
+combined_data4.to_csv("datasets\combined_data_Sport_Hall_71_loc0000.csv", index=False)
+
+# List of CSV files to combine
+csv_files = [
+    'datasets\combined_data_Corridor_rm155_71_loc0000.csv',
+    'datasets\combined_data_Lab139_71_loc0000.csv',
+    'datasets\combined_data_Main_Lobby71_loc0000.csv',
+    'datasets\combined_data_Sport_Hall_71_loc0000.csv'
+]
+
+# combine all combined data into a single dataframe
+data_frames = [combined_data1, combined_data2, combined_data3, combined_data4]
+
+combined_data = pd.concat(data_frames, ignore_index=True)
+
+combined_data.to_csv("combined_data_all.csv", index=False)
+
+# drop the 5th collumn
+combined_data = combined_data.drop(columns=[5])
+print("dropping 5th col")
+print(combined_data)
+
+# Assuming you have loaded your data into the combined_data DataFrame
+# If not, you should load your data here
+
+# Handling missing values by dropping rows with missing values
+combined_data.dropna(inplace=True)
+print("dropping na")
+print(combined_data)
+
+# Splitting into X (features) and y (labels)
+X = combined_data.drop(columns=['label'])  # Assuming 'label' is the column containing your labels
+print("X")
+print(X)
+
+y = combined_data['label']
+print("y")
+print(y)
 
 
-
-
-#print(df.head())
-
-def extract_label(file_name):
-    if 'Corridor_rm155_71_loc0000_path' in file_name:
-        return 'Corridor'
-    elif 'Lab139_71_loc0000_path' in file_name:
-        return 'Lab'
-    elif 'Main_Lobby71_loc0000_path' in file_name:
-        return 'Main Lobby'
-    elif 'Sport_Hall_71_loc0000_path' in file_name:
-        return "Sport Hall"
-    else:
-        return None
 '''
-print(df.head())
-# Open the CSV file with semicolon as the delimiter
-with open(loc0000csvfile, 'r') as file:
-    # Create a CSV reader object with semicolon delimiter
-    csv_reader = csv.reader(file, delimiter=';')
-    
-    # Iterate through the rows in the CSV file
-    for row in csv_reader:
-        # Each 'row' is a list of values in that row
-        # Access the values by their index
-        # For example, row[0] gets the first value in the row
-        print(row)
+
+# Splitting into X (features) and y (labels)
+X = combined_data.drop(columns=['label'])  # Assuming 'label' is the column containing your labels
+X = pd.get_dummies(X)  # One-hot encoding categorical columns
+y = combined_data['label']
+
+# Splitting into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Creating an SVM model and training it
+svm_model = SVC(kernel='linear', C=1.0, random_state=42)  # You can adjust the kernel and C value as needed
+svm_model.fit(X_train, y_train)
+
+# Making predictions on the test data
+y_pred = svm_model.predict(X_test)
+
+# Evaluating the model's accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
 '''
