f12f1c9bebb0d80e939146fcdf0432da90e22744
evanreidsmith7 <evanreidsmith7@gmail.com>
2023-09-15 12:00:05 -0500

    need to extract the most influential

--- HW/HW2/SmithEvanEE4331HW2/hw2-svm.py
+++ HW/HW2/SmithEvanEE4331HW2/hw2-svm.py
@@ -22,6 +22,8 @@
 # Import libraries
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
@@ -34,6 +36,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 # * labels *
 # corridor_rm155_7.1
 # Lab139_7.1
@@ -45,6 +48,8 @@ Corridor_rm155_71_loc0000_path = 'datasets\Measurements_Upload\Measurements_Uplo
 Lab139_71_loc0000_path = 'datasets\Measurements_Upload\Measurements_Upload\Corridor_rm155_7.1\Loc_0000'
 Main_Lobby71_loc0000_path = 'datasets\Measurements_Upload\Measurements_Upload\Corridor_rm155_7.1\Loc_0000'
 Sport_Hall_71_loc0000_path = 'datasets\Measurements_Upload\Measurements_Upload\Corridor_rm155_7.1\Loc_0000'
+RESULTS_ACCURACY_LOCATION = 'HW\HW2\SmithEvanEE4331HW2\Part1\part1results.txt'
+
 
 # Create a dataframe to store the data
 combined_data = pd.DataFrame()
@@ -115,45 +120,152 @@ combined_data.to_csv("combined_data_all.csv", index=False)
 
 # drop the 5th collumn
 combined_data = combined_data.drop(columns=[5])
-print("dropping 5th col")
-print(combined_data)
 
 # Assuming you have loaded your data into the combined_data DataFrame
 # If not, you should load your data here
 
 # Handling missing values by dropping rows with missing values
 combined_data.dropna(inplace=True)
-print("dropping na")
-print(combined_data)
 
 # Splitting into X (features) and y (labels)
 X = combined_data.drop(columns=['label'])  # Assuming 'label' is the column containing your labels
-print("X")
-print(X)
 
+#y = combined_data.drop(columns=[0,1,2,3,4])
 y = combined_data['label']
-print("y")
-print(y)
+
+# encode y
+le = LabelEncoder()
+y = le.fit_transform(y)
 
 
-'''
+# standardize X
+sc = StandardScaler()
+sc.fit(X)
+X = sc.transform(X)
 
-# Splitting into X (features) and y (labels)
-X = combined_data.drop(columns=['label'])  # Assuming 'label' is the column containing your labels
-X = pd.get_dummies(X)  # One-hot encoding categorical columns
-y = combined_data['label']
+
 
 # Splitting into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Creating an SVM model and training it
-svm_model = SVC(kernel='linear', C=1.0, random_state=42)  # You can adjust the kernel and C value as needed
-svm_model.fit(X_train, y_train)
+# Apply PCA with two components to the training data
+n_components_pca = 2  # Adjust the number of components as needed
+pca = PCA(n_components=n_components_pca)
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.transform(X_test)
 
-# Making predictions on the test data
-y_pred = svm_model.predict(X_test)
+###########################################################################################################################
+# TRAINING AND TESTING THE PCA svc MODEL
+###########################################################################################################################
 
-# Evaluating the model's accuracy
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
+# define the parameters to use in the grid search
+param_grid = {
+    'C': [0.1, 1, 10, 100, 1000]
+}
+
+# use the grid search to find the best parameters
+model = SVC(random_state=42)
+
+# create a grid search object
+grid_search_object = GridSearchCV(model, param_grid, cv=5)
+
+# fit the grid search object to the training data
+grid_search_object.fit(X_train_pca, y_train)
+
+# get the best esiimator and its test accuracy
+best_estimator = grid_search_object.best_estimator_
+
+# get the best parameters
+best_parameters = grid_search_object.best_params_
+
+# lets give scv1 the best parameters
+model1 = SVC(
+    C=best_parameters['C'], kernel='linear', random_state=42)
+
+# train the model
+model1.fit(X_train_pca, y_train)
+y_train_pred = model1.predict(X_train_pca)
+y_pred = model1.predict(X_test_pca)
+
+# Train the best model on the entire training dataset (X_train_scaled, y_train)
+best_model = model1  # Change this to your best model
+
+# Make predictions on the entire training and testing datasets
+y_train_pred = best_model.predict(X_train_pca)  # Change to X_train_lda if using LDA
+y_test_pred = best_model.predict(X_test_pca)    # Change to X_test_lda if using LDA
+
+# Calculate training and testing accuracy
+train_accuracy = accuracy_score(y_train, y_train_pred)
+test_accuracy = accuracy_score(y_test, y_test_pred)
+
+# Print and save the accuracy values to a text file
+accuracy_text = f"Training Accuracy: {train_accuracy:.2f}\nTesting Accuracy: {test_accuracy:.2f}\n"
+print(accuracy_text)
+
+
+# Access the principal components (eigenvectors) and their importance
+principal_components = pca.components_
+explained_variance = pca.explained_variance_ratio_
+
+feature_names = ['cooridor', 'lab139', 'main_lobby', 'sport_hall']
+
+# Print the principal components and their importance
+for i, component in enumerate(principal_components):
+    print(f"Principal Component {i + 1}:")
+    print(component)
+    print(f"Explained Variance: {explained_variance[i]:.4f}\n")
+    for(feature) in component:
+        print(f"Feature {feature}")
+    
+
+
+
+
+
+
+
+
+
+best_pcComps = f"Best PCA Components: {pca.components_}\n"
+
+
+# Save the accuracy and feature information to a text file
+with open('HW\HW2\SmithEvanEE4331HW2\Part1\part1results.txt', 'w') as file:
+    file.write(accuracy_text)
+    file.write(best_pcComps)
+
+#best features 1 and 3
+
+
+'''
+# Train the best model on the entire training dataset (X_train_scaled, y_train)
+best_model = svm_model_pca  # Change this to your best model
+
+# Make predictions on the entire training and testing datasets
+y_train_pred = best_model.predict(X_train_pca)  # Change to X_train_lda if using LDA
+y_test_pred = best_model.predict(X_test_pca)    # Change to X_test_lda if using LDA
+
+# Calculate training and testing accuracy
+train_accuracy = accuracy_score(y_train, y_train_pred)
+test_accuracy = accuracy_score(y_test, y_test_pred)
+
+# Print and save the accuracy values to a text file
+accuracy_text = f"Training Accuracy: {train_accuracy:.2f}\nTesting Accuracy: {test_accuracy:.2f}\n"
+print(accuracy_text)
+
+# Save the best features from PCA or LDA to a text file
+if isinstance(best_model, PCA):
+    best_features = f"Best PCA Components: {best_model.components_}\n"
+elif isinstance(best_model, LDA):
+    best_features = f"Best LDA Components: {best_model.scalings_}\n"
+else:
+    best_features = "No feature information available for this model.\n"
+
+print(best_features)
+
+# Save the accuracy and feature information to a text file
+with open('experiment_results.txt', 'w') as file:
+    file.write(accuracy_text)
+    file.write(best_features)
+
 '''
