4c79c396788c630e65715cb2c71a25df8886c40a
Evan R Smith <ers131@login2.local>
2023-10-13 20:18:35 -0500

    update

--- HW/HW4/SmithEvanEE4331HW4/Part1/hw4-part1.py
+++ HW/HW4/SmithEvanEE4331HW4/Part1/hw4-part1.py
@@ -22,34 +22,43 @@ from sklearn.cluster import KMeans
 print("Data Preprocessing...")
 ###########################################################################################################################
 # Specify the path to your CSV file
-csv_file_path = "Dataset/gt_2011.csv"
+csv_file_path_2011 = "Dataset/gt_2011.csv"
+csv_file_path_2012 = "Dataset/gt_2012.csv"
+csv_file_path_2013 = "Dataset/gt_2013.csv"
+csv_file_path_2014 = "Dataset/gt_2014.csv"
+csv_file_path_2015 = "Dataset/gt_2015.csv"
+
+# image paths
+distortion_clusters_file_path = "Part1/Results/distortion_clusters.png"
+
+clusters_file_path_2011 = "Part1/Results/2011clusters.png"
+clusters_file_path_2012 = "Part1/Results/2012clusters.png"
+clusters_file_path_2013 = "Part1/Results/2013clusters.png"
+clusters_file_path_2014 = "Part1/Results/2014clusters.png"
+clusters_file_path_2015 = "Part1/Results/2015clusters.png"
+
+silhouettes_file_path_2011 = "Part1/Results/2011silhoute.png"
+silhouettes_file_path_2011 = "Part1/Results/2012silhoute.png"
+silhouettes_file_path_2011 = "Part1/Results/2013silhoute.png"
+silhouettes_file_path_2011 = "Part1/Results/2014silhoute.png"
+silhouettes_file_path_2011 = "Part1/Results/2015silhoute.png"
+
 
 # Use the pandas.read_csv() function to read the CSV file into a DataFrame
-df = pd.read_csv(csv_file_path)
-print(df.head())
+df_2011 = pd.read_csv(csv_file_path_2011, header=None)
+df_2012 = pd.read_csv(csv_file_path_2012, header=None)
+df_2013 = pd.read_csv(csv_file_path_2013, header=None)
+df_2014 = pd.read_csv(csv_file_path_2014, header=None)
+df_2015 = pd.read_csv(csv_file_path_2015, header=None)
+print(df_2011.head())
 
 
 ##########################################################################################################################
 print("Data Preprocessing done.")
 # Determine the number of centroids by using the Elbow Method
 print("Determining the number of centroids by using the Elbow Method...")
-###example#############################################################
-'''
-distortions = []
-for i in range(1, 11):
-... km = KMeans(n_clusters=i,
-... init='k-means++',
-... n_init=10,
-... max_iter=300,
-... random_state=0)
-km.fit(X)
-distortions.append(km.inertia_)
-plt.plot(range(1,11), distortions, marker='o')
-plt.xlabel('Number of clusters')
-plt.ylabel('Distortion')
-plt.show()
-'''
 ###########################################################################################################################
+
 distortions = []
 for i in range(1, 11):
     km = KMeans(n_clusters=i,
@@ -57,5 +66,22 @@ for i in range(1, 11):
                 n_init=10,
                 max_iter=300,
                 random_state=0)
-    km.fit(df)
+    km.fit(df_2011)
     distortions.append(km.inertia_)
+
+##########################################################################################################################
+print("done.")
+# plot the distortion
+print("plotting distortions...")
+##########################################################################################################################
+
+plt.plot(range(1,11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.savefig(distortion_clusters_file_path)
+
+##########################################################################################################################
+print("done.")
+# plot the clusters
+print("plotting clusters...")
+##########################################################################################################################
