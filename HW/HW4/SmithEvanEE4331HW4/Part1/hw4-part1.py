0a576e9667811e533cb6363f8075824f17084369
Evan R Smith <ers131@login2.local>
2023-10-13 19:18:31 -0500

    asdf

--- HW/HW4/SmithEvanEE4331HW4/Part1/hw4-part1.py
+++ HW/HW4/SmithEvanEE4331HW4/Part1/hw4-part1.py
@@ -16,7 +16,7 @@
 import os
 from matplotlib import pyplot as plt
 import pandas as pd
-
+from sklearn.cluster import KMeans
 ##########################################################################################################################
 # Data Preprocessing
 print("Data Preprocessing...")
@@ -27,3 +27,35 @@ csv_file_path = "Dataset/gt_2011.csv"
 # Use the pandas.read_csv() function to read the CSV file into a DataFrame
 df = pd.read_csv(csv_file_path)
 print(df.head())
+
+
+##########################################################################################################################
+print("Data Preprocessing done.")
+# Determine the number of centroids by using the Elbow Method
+print("Determining the number of centroids by using the Elbow Method...")
+###example#############################################################
+'''
+distortions = []
+for i in range(1, 11):
+... km = KMeans(n_clusters=i,
+... init='k-means++',
+... n_init=10,
+... max_iter=300,
+... random_state=0)
+km.fit(X)
+distortions.append(km.inertia_)
+plt.plot(range(1,11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.show()
+'''
+###########################################################################################################################
+distortions = []
+for i in range(1, 11):
+    km = KMeans(n_clusters=i,
+                init='k-means++',
+                n_init=10,
+                max_iter=300,
+                random_state=0)
+    km.fit(df)
+    distortions.append(km.inertia_)
